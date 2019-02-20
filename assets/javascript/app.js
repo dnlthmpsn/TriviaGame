03db42b9b1ae676e679d4b12b9726233dcad9719
Daniel Thompson <1dptt@queensu.ca>
2019-02-20 12:05:19 -0500

    psuedo psuedo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,56 @@
 alert("connected");
+
+//Global Variables
+//========================
+
+//array of objects, each object is a trivia question. question, correctResponse, asset to be served on correct/incorrect. question difficulty increasing 
+    //answer is always a single option: can be fill in the blank, true, false, or one of multiple options. 
+
+var masterTimer;
+    
+//DOM element selectors
+    //user input: key listeners and submit on enter.
+
+
+
+//Functions 
+//========================
+
+function keepScore(){
+    //correct/inccorect in a scoreboard. animation included. beyond simple..
+}
+
+
+function moveOn(){
+     //after set amount of time
+    //if user hits next question on the showQuestionResult screen
+}
+
+function nextQuestion(){
+     //function timer: they start with 10 seconds, and the next question adds the time remaining from a correct previous responds. 
+    //function checkInput: if user input matches correctResponse, render correctAsset, ifNot render correct answer and incorrectAsset
+
+}
+
+function newGame(){
+    //begin master timer
+    nextQuestion();
+
+}
+   
+    
+function showQuestionResult(){
+ //render the appropriate content for correct/incorrect response
+ moveOn();
+}
+       
+
+//function renderGameResults: after all questions are responded to
+    //function newGame
+
+
+
+
+
+//Main Process
+//========================
